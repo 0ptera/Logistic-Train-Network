38bcede25af9369a2fa9d5ba5f49157e9e23377c
Optera <yousei.kyoko+github@gmail.com>
2018-12-09 08:51:03 +0100

    Add files via upload

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,37 +1,23 @@
 
 local function createIcons(entity)
-  if entity.icon and entity.icon_size then
-    local icons = {
-      {
-        icon = entity.icon,
-        icon_size = entity.icon_size,
-        tint = {r=1, g=1, b=1, a=1}
-      },
-      {
-        icon = "__LogisticTrainNetwork__/graphics/icons/encoded-position.png",
-        icon_size = 32,
-        tint = {r=1, g=1, b=1, a=1}
-      },
-    }
+  if entity.icons then
+    local icons = {}
+    for k,v in pairs(entity.icons) do
+      -- assume every other mod is written lazy lacking full prototype definitions
+      icons[#icons+1] = { icon = v.icon, icon_size = v.icon_size or entity.icon_size or 32, tint = v.tint }
+    end
+    icons[#icons+1] = { icon = "__LogisticTrainNetwork__/graphics/icons/encoded-position.png", icon_size = 32, tint = {r=1, g=1, b=1, a=1} }
     -- log(serpent.block(icons))
     return icons
-  elseif entity.icons then
-    local icons = entity.icons
-    table.insert(icons, {
-      icon = "__LogisticTrainNetwork__/graphics/icons/encoded-position.png",
-      icon_size = 32,
-      tint = {r=1, g=1, b=1, a=1}
-    })
+  elseif entity.icon then
+    local icons = {
+      { icon = entity.icon, icon_size = entity.icon_size or 32, tint = {r=1, g=1, b=1, a=1} },
+      { icon = "__LogisticTrainNetwork__/graphics/icons/encoded-position.png", icon_size = 32, tint = {r=1, g=1, b=1, a=1} }
+    }
     -- log(serpent.block(icons))
     return icons
   else
-    local icons = {
-      {
-        icon = "__LogisticTrainNetwork__/graphics/icons/encoded-position.png",
-        icon_size = 32,
-        tint = {r=1, g=1, b=1, a=1}
-      }
-    }
+    local icons = { icon = "__LogisticTrainNetwork__/graphics/icons/encoded-position.png", icon_size = 32, tint = {r=1, g=1, b=1, a=1} }
     -- log(serpent.block(icons))
     return icons
   end
@@ -44,7 +30,7 @@ for _, loco in pairs(data.raw["locomotive"]) do
     type = "virtual-signal",
     name = "LTN-"..loco.name,
     icons = createIcons(loco),
-    icon_size = 32,
+    icon_size = nil,
     subgroup = "LTN-signal",
     order = "z[LTN-signal]-u"..string.format("%02d", lococount),
     localised_name = {"virtual-signal-name.LTN-locomotive", {"entity-name." .. loco.name}}
@@ -59,7 +45,7 @@ for _, wagon in pairs(data.raw["cargo-wagon"]) do
     type = "virtual-signal",
     name = "LTN-"..wagon.name,
     icons = createIcons(wagon),
-    icon_size = 32,
+    icon_size = nil,
     subgroup = "LTN-signal",
     order = "z[LTN-signal]-v"..string.format("%02d", wagoncount),
     localised_name = {"virtual-signal-name.LTN-wagon", {"entity-name." .. wagon.name}}
@@ -72,7 +58,7 @@ for _, wagon in pairs(data.raw["fluid-wagon"]) do
     type = "virtual-signal",
     name = "LTN-"..wagon.name,
     icons = createIcons(wagon),
-    icon_size = 32,
+    icon_size = nil,
     subgroup = "LTN-signal",
     order = "z[LTN-signal]-v"..string.format("%02d", wagoncount),
     localised_name = {"virtual-signal-name.LTN-wagon", {"entity-name." .. wagon.name}}
@@ -85,7 +71,7 @@ for _, wagon in pairs(data.raw["artillery-wagon"]) do
     type = "virtual-signal",
     name = "LTN-"..wagon.name,
     icons = createIcons(wagon),
-    icon_size = 32,
+    icon_size = nil,
     subgroup = "LTN-signal",
     order = "z[LTN-signal]-v"..string.format("%02d", wagoncount),
     localised_name = {"virtual-signal-name.LTN-wagon", {"entity-name." .. wagon.name}}
