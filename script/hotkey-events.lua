b29caa03d49312301edb13a962a2a35edff7a7d3
houseofsteinert <14843079+StephanSteinert@users.noreply.github.com>
2020-10-03 17:54:18 +0200

    Made requested changes
    - changed hotkeys.lua and hotkey-events.lua to 2 space intendation
    - changed localisation

--- script/hotkey-events.lua
+++ script/hotkey-events.lua
@@ -1,12 +1,12 @@
 script.on_event("ltn-toggle-dispatcher", function(event)
-    local player = game.get_player(event.player_index)
+  local player = game.get_player(event.player_index)
 
-    local enabled = settings.global["ltn-dispatcher-enabled"].value
-    if enabled then
-        settings.global["ltn-dispatcher-enabled"] = {value = false}
-        printmsg({"ltn-message.dispatcher-disabled", player.name}, nil, false)
-    else
-        settings.global["ltn-dispatcher-enabled"] = {value = true}
-        printmsg({"ltn-message.dispatcher-enabled", player.name}, nil, false)
-    end
+  local enabled = settings.global["ltn-dispatcher-enabled"].value
+  if enabled then
+    settings.global["ltn-dispatcher-enabled"] = {value = false}
+    printmsg({"ltn-message.dispatcher-disabled", player.name}, nil, false)
+  else
+    settings.global["ltn-dispatcher-enabled"] = {value = true}
+    printmsg({"ltn-message.dispatcher-enabled", player.name}, nil, false)
+  end
 end)
