fe79144f33f9fc0b39609ad90d4856dfec323f76
houseofsteinert <14843079+StephanSteinert@users.noreply.github.com>
2020-10-03 17:49:51 +0200

    Made requested changes
    - changed hotkeys.lua and hotkey-events.lua to 2 space intendation
    - changed localisation

--- prototypes/hotkeys.lua
+++ prototypes/hotkeys.lua
@@ -1,8 +1,8 @@
 data:extend{
-	{
-		type = "custom-input",
-		name = "ltn-toggle-dispatcher",
-		key_sequence = "CONTROL + SHIFT + D",
-		consuming = "game-only"
-	}
+  {
+    type = "custom-input",
+    name = "ltn-toggle-dispatcher",
+    key_sequence = "CONTROL + SHIFT + D",
+    consuming = "game-only"
+  }
 }
